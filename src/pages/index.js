e9490b10c39b676bf7c2ad2622d89c6ee48d2a08
Clebson Augusto Fonseca <43012757+whoisclebs@users.noreply.github.com>
2023-06-01 22:03:45 -0300

    chore: refactor @ in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="py-8 max-w-[450px] border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-1 hover:shadow transition duration-200 bg-white flex-shrink-0">
       <img className="w-24 h-24 rounded-full mx-auto bg-slate-400" src="/avatar.png" alt="Clebson Avatar"/>
       <div className="text-gray-700 text-xs pt-4 text-center">
-        <h1 className="text-slate-900 text-3xl">@clebsonf</h1>
+        <h1 className="text-slate-900 text-3xl">@whoisclebs</h1>
         <h2 className="pt-4 text-xl">Clebson Augusto Fonseca</h2>
         <p className="pt-2 text-justify text-sm">
           🚀 Engenheiro de software, professor, palestrante e criador de conteúdo. Atualmente sou professor particular e meu foco tem sido distribuir conhecimento e contribuir no open source.
@@ -22,7 +22,7 @@ export default function Home() {
               </Link>
           </li>
           <li className="pt-2 text-sm">
-              <Link href="https://www.buymeacoffee.com/clebsonf">
+              <Link href="https://www.buymeacoffee.com/whoisclebs">
                 <button type="button" className="relative px-6 py-2 group w-full">
                   <span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0" />
                   <span className="absolute inset-0 w-full h-full bg-white border-2 border-black " />
@@ -31,7 +31,7 @@ export default function Home() {
               </Link>
           </li>
           <li className="pt-2 text-sm">
-              <Link href="https://clebson.cc">
+              <Link href="https://whoisclebs.space">
                 <button type="button" className="relative px-6 py-2 group w-full">
                   <span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0" />
                   <span className="absolute inset-0 w-full h-full bg-white border-2 border-black " />
@@ -41,25 +41,25 @@ export default function Home() {
           </li>
         </ul>
         <div className="flex pt-4 space-x-6 justify-center">
-          <a href="https://github.com/clebsonf" className="text-gray-500 hover:text-gray-900">
+          <a href="https://github.com/whoisclebs" className="text-gray-500 hover:text-gray-900">
             <FaGithub size={25}/>
           </a>
-          <a href="https://dev.to/clebsonf" className="text-gray-500 hover:text-gray-900">
+          <a href="https://dev.to/whoisclebs" className="text-gray-500 hover:text-gray-900">
             <FaDev size={25}/>
           </a>
-          <a href="https://twitter.com/cl3bsonf" className="text-gray-500 hover:text-gray-900">
+          <a href="https://twitter.com/whoisclebs" className="text-gray-500 hover:text-gray-900">
             <FaTwitter size={25}/>
           </a>
           <a href="https://medium.com/@clebson" className="text-gray-500 hover:text-gray-900">
             <FaMedium size={25}/>
           </a>
-          <a href="https://www.instagram.com/cl3bsonf/" className="text-gray-500 hover:text-gray-900">
+          <a href="https://www.instagram.com/whoisclebs/" className="text-gray-500 hover:text-gray-900">
             <FaInstagram size={25}/>
           </a>
-          <a href="https://www.youtube.com/@clebsonf" className="text-gray-500 hover:text-gray-900">
+          <a href="https://www.youtube.com/@whoisclebs" className="text-gray-500 hover:text-gray-900">
             <FaYoutube size={25}/>
           </a>
-          <a href="https://www.linkedin.com/in/clebsonf/" className="text-gray-500 hover:text-gray-900">
+          <a href="https://www.linkedin.com/in/whoisclebs/" className="text-gray-500 hover:text-gray-900">
             <FaLinkedin size={25}/>
           </a>
         </div>
